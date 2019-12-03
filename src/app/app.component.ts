9e58c1007b04071041205c9b589016bd83e91bc6
test <khanhnguyen.ict@gmail.com>
2019-12-03 16:58:19 +0700

    example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as googleAuthen from 'google-authen';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-app';
+
+export class AppComponent implements OnInit {
+  script = { src: 'https://apis.google.com/js/platform.js?onload=init' };
+  eventEmmiter = googleAuthen.eventEmitter;
+  googleSignInClient: string = '146277941132-51uhovmfh86i030igjolofcpav2jtkt6.apps.googleusercontent.com';
+  ngOnInit() {
+    googleAuthen.handleInitGoogleApi(this.script, this.googleSignInClient, 'gsuite-btn');
+    this.eventEmmiter.on('sign-in-success', (authInfo) => {
+      this.login(authInfo);
+    });
+  }
+  login(data) {
+    debugger;
+    console.log(data);
+  }
 }
+
+
